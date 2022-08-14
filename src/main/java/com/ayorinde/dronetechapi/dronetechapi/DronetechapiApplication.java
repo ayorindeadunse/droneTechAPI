811b619bb109ba9e9f6b5cd88ac55474ffaa6468
adunseayorinde <adunseayorinde@gmail.com>
2022-08-14 23:09:53 +0100

    Initialized droneTechApi project

---
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/DronetechapiApplication.java
@@ -0,0 +1,13 @@
+package com.ayorinde.dronetechapi.dronetechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronetechapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronetechapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayorinde/dronetechapi/dronetechapi/DronetechapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayorinde.dronetechapi.dronetechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronetechapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
