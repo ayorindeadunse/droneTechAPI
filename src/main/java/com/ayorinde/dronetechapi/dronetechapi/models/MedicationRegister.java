cefe02fb9d87d223db1bf4b4e94d4c18b807099d
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 00:25:26 +0100

    added MedicationRegister entity model

---
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/models/MedicationRegister.java
@@ -0,0 +1,29 @@
+package com.ayorinde.dronetechapi.dronetechapi.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "medicationregister")
+public class MedicationRegister {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Long Id; // primary key
+    @Column(name = "medicationCode")
+    private String medicationCode; //make this a unique field
+    @Column(name = "createdDate")
+    private Date createdDate;
+    @Column(name = "modifiedDate")
+    private Date modifiedDate;
+}
+
