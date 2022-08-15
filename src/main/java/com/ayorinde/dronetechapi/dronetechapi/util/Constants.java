33f4e6f92a25d752eab000bf68d14c19aa914471
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 00:29:16 +0100

    added Constants util class

---
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/util/Constants.java
@@ -0,0 +1,6 @@
+package com.ayorinde.dronetechapi.dronetechapi.util;
+
+public class Constants {
+    public static final int BATTERY_CAPACITY = 100;
+    public static final int DRONE_WEIGHT = 500;
+}
