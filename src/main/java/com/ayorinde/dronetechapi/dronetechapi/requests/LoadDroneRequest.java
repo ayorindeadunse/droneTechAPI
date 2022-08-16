cb9a5f2de9d232c71f263ebf8d8d1bd7a56aef3a
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 18:30:01 +0100

    added LoadDroneRequest POJO

---
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/requests/LoadDroneRequest.java
@@ -0,0 +1,15 @@
+package com.ayorinde.dronetechapi.dronetechapi.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class LoadDroneRequest {
+
+    private String serialNumber;
+    private List<String> medicineCode;
+
+}
