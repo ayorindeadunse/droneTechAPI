53a95cbb15d13165e3c222f6f90745429c5f5c90
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 09:12:00 +0100

    updated DroneRepositoy

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/repositories/DroneRepository.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/repositories/DroneRepository.java
@@ -4,4 +4,9 @@ import com.ayorinde.dronetechapi.dronetechapi.models.Drone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DroneRepository extends JpaRepository<Drone,Long> {
+  //  @Query(value ="SELECT DRONE_WEIGHT from DRONES WHERE SERIAL_NUMBER = ?1",nativeQuery = true)
+    int getDroneWeight(String serialNumber);
+
+  //  @Query(value = "SELECT * FROM DRONES WHERE SERIAL_NUMBER = ?1",nativeQuery = true)
+    Drone checkIfDroneExists(String serialNumber);
 }
