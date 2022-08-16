3dfb298313faeea732b019aaa4b374c2d5686d5f
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 21:22:43 +0100

    added registerDrone and getSelectedDroneState contracts

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/services/DroneService.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/services/DroneService.java
@@ -1,8 +1,10 @@
 package com.ayorinde.dronetechapi.dronetechapi.services;
 
 import com.ayorinde.dronetechapi.dronetechapi.models.Drone;
+import com.ayorinde.dronetechapi.dronetechapi.models.DroneState;
 import com.ayorinde.dronetechapi.dronetechapi.requests.DroneRegistrationRequest;
 
 public interface DroneService {
     Drone registerDrone(DroneRegistrationRequest drone); //get drone serialNumber
+    DroneState getSelectedDroneState(String serialNumber);//get the drone state. Use a switchcase method to return the droneState
 }
