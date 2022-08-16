5fce08e9fe8d6b4d604ac2649b97c9beea045413
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 21:24:06 +0100

    added getBatteryLevel contract

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/services/DroneService.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/services/DroneService.java
@@ -7,4 +7,5 @@ import com.ayorinde.dronetechapi.dronetechapi.requests.DroneRegistrationRequest;
 public interface DroneService {
     Drone registerDrone(DroneRegistrationRequest drone); //get drone serialNumber
     DroneState getSelectedDroneState(String serialNumber);//get the drone state. Use a switchcase method to return the droneState
+    int getBatteryLevel(String serialNumber); //get the battery level for a particular drone from event log.
 }
