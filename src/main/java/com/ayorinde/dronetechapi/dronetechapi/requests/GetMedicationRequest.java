93ea58552252a6688ff98ee4a642bc2bc5bfb1b4
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 18:37:00 +0100

    added GetMedicationRequest POJO

---
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/requests/GetMedicationRequest.java
@@ -0,0 +1,10 @@
+package com.ayorinde.dronetechapi.dronetechapi.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetMedicationRequest {
+    private String SerialNumber;
+}
