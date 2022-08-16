36e4c866f1c3c31243ee432077ccded80554168a
adunseayorinde <adunseayorinde@gmail.com>
2022-08-16 18:27:16 +0100

    Added MedicationRepository

---
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/repositories/MedicationRepository.java
@@ -0,0 +1,14 @@
+package com.ayorinde.dronetechapi.dronetechapi.repositories;
+
+import com.ayorinde.dronetechapi.dronetechapi.models.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface MedicationRepository extends JpaRepository<Medication,Long> {
+
+    @Query(value = "SELECT MEDICINE_WEIGHT from MEDICATIONS where CODE = ?1",nativeQuery = true)
+    int medicationWeight(String medicationCode);
+
+    @Query(value = "SELECT * FROM MEDICATIONS WHERE CODE = ?1",nativeQuery = true)
+    Medication checkIfMedicationExists(String code);
+}
