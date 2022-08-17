e0d13bfba56fd503a8a7feddabd49d8d67593c8d
adunseayorinde <adunseayorinde@gmail.com>
2022-08-17 13:22:54 +0100

    added registerDrone method body

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/services/DroneServiceImpl.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/services/DroneServiceImpl.java
@@ -1,18 +1,17 @@
 package com.ayorinde.dronetechapi.dronetechapi.services;
 
 import com.ayorinde.dronetechapi.dronetechapi.exceptions.DroneException;
-import com.ayorinde.dronetechapi.dronetechapi.models.Drone;
-import com.ayorinde.dronetechapi.dronetechapi.models.DroneState;
-import com.ayorinde.dronetechapi.dronetechapi.models.EventLog;
-import com.ayorinde.dronetechapi.dronetechapi.models.LoadDrone;
+import com.ayorinde.dronetechapi.dronetechapi.models.*;
 import com.ayorinde.dronetechapi.dronetechapi.repositories.*;
 import com.ayorinde.dronetechapi.dronetechapi.requests.DroneRegistrationRequest;
 import com.ayorinde.dronetechapi.dronetechapi.requests.GetDroneBatteryLevelRequest;
 import com.ayorinde.dronetechapi.dronetechapi.requests.GetMedicationRequest;
 import com.ayorinde.dronetechapi.dronetechapi.requests.LoadDroneRequest;
+import com.ayorinde.dronetechapi.dronetechapi.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -39,10 +38,43 @@ public class DroneServiceImpl implements DroneService,MedicationService{
     public Drone registerDrone(DroneRegistrationRequest droneRegistrationRequest) {
         // check if the drone exists
         Drone drone, drone1;
+        DroneRegister droneRegister;
         String serialNumber = droneRegistrationRequest.setSerialNumber(); //create unique serial number for drone registration.
         try
         {
             Drone d = checkDroneExists(serialNumber);
+            if(d != null)
+            {
+                System.out.println("Drone with Serial Number "+serialNumber +" already exists.");
+            }
+            drone = new Drone();
+            drone.setSerialNumber(serialNumber);
+            drone.setDroneModel(droneRegistrationRequest.getDroneModel());
+            drone.setDroneWeight(droneRegistrationRequest.getDroneWeight());
+            drone.setBatteryCapacity(Constants.BATTERY_CAPACITY);
+            drone.setDroneState(DroneState.IDLE);
+
+            drone1 = droneRepository.save(drone);
+            if(drone1.getSerialNumber() != null)
+            {
+                // save  in DroneRegister table
+                droneRegister = new DroneRegister();
+                droneRegister.setSerialNumber(drone1.getSerialNumber());
+                droneRegister.setCreatedDate(new Date());
+                droneRegister.setModifiedDate(new Date());
+
+                DroneRegister dr1 = droneRegisterRepository.save(droneRegister);
+                if(dr1.getId() > 0)
+                {
+                    System.out.println("Drone Successfully Registered");
+                    //Log state in event log
+                    String auditEntry = "Drone Registered";
+                    addEventLog(drone1,auditEntry);
+                }
+
+
+            }
+
 
         }
         catch(Exception e)
@@ -53,6 +85,18 @@ public class DroneServiceImpl implements DroneService,MedicationService{
         return null;
     }
 
+    void addEventLog(Drone dr,String auditEntry)
+    {
+       EventLog eventLog = new EventLog();
+       eventLog.setSerialNumber(dr.getSerialNumber());
+       eventLog.setDroneState(dr.getDroneState());
+       eventLog.setBatteryLevel(dr.getBatteryCapacity());
+       eventLog.setDateCreated(new Date());
+       eventLog.setDateModified(new Date());
+
+       EventLog eventLog1 = eventLogRepository.save(eventLog);
+       System.out.println("New Audit log created "+ auditEntry+ ". Log entry: "+eventLog1.getId());
+    }
     @Override
     public DroneState getSelectedDroneState(String serialNumber) {
         return null;
