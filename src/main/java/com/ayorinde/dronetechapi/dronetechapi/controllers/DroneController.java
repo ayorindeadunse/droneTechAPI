3e7f8a9227a6b006bfd83e0878924cae68212627
adunseayorinde <adunseayorinde@gmail.com>
2022-08-18 12:25:37 +0100

    added loaddrone endpoint implementation

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/controllers/DroneController.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/controllers/DroneController.java
@@ -1,14 +1,21 @@
 package com.ayorinde.dronetechapi.dronetechapi.controllers;
 
+import com.ayorinde.dronetechapi.dronetechapi.exceptions.DroneException;
 import com.ayorinde.dronetechapi.dronetechapi.models.Drone;
+import com.ayorinde.dronetechapi.dronetechapi.models.DroneState;
+import com.ayorinde.dronetechapi.dronetechapi.models.LoadDrone;
 import com.ayorinde.dronetechapi.dronetechapi.requests.DroneRegistrationRequest;
+import com.ayorinde.dronetechapi.dronetechapi.requests.LoadDroneRequest;
 import com.ayorinde.dronetechapi.dronetechapi.responses.ApiResponse;
 import com.ayorinde.dronetechapi.dronetechapi.services.DroneServiceImpl;
 import com.ayorinde.dronetechapi.dronetechapi.util.Constants;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DroneController {
@@ -25,7 +32,22 @@ public class DroneController {
             return ResponseEntity.badRequest().body(new ApiResponse(false, "Maximum Drone Weight is " + Constants.DRONE_WEIGHT, droneRegistrationRequest));
         }
         Drone drone = droneService.registerDrone(droneRegistrationRequest);
-        //return ResponseEntity.ok(d);
         return ResponseEntity.ok(new ApiResponse(true, "Drone registered successfully!", drone));
     }
+    @PostMapping("/api/droneTech/loaddrone")
+    public ResponseEntity LoadDrone(@RequestBody LoadDroneRequest loadDroneRequest) throws DroneException {
+        List<LoadDrone> l;
+        l = droneService.loadDrone(loadDroneRequest);
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getDroneState() == DroneState.IDLE) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(false, "Battery Level is too low for this operation", l));
+            }
+            return ResponseEntity.ok(new ApiResponse(true, "Drone Loaded Successfully!", l));
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(false, "An Error occurred on the server", l));
+
+    }
+
+
 }
