0d460aa86b6c1860c2b4faaeee0418ee326bb234
adunseayorinde <adunseayorinde@gmail.com>
2022-08-18 09:02:15 +0100

    updating DroneController

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/controllers/DroneController.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/controllers/DroneController.java
@@ -1,4 +1,14 @@
 package com.ayorinde.dronetechapi.dronetechapi.controllers;
 
+import com.ayorinde.dronetechapi.dronetechapi.services.DroneServiceImpl;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DroneController {
+    private final DroneServiceImpl droneService;
+
+    public DroneController(DroneServiceImpl droneService) {
+        this.droneService = droneService;
+    }
+
 }
