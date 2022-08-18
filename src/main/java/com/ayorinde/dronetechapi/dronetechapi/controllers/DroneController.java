7077a5ea37d7ba77db0995082409c08a868a3986
adunseayorinde <adunseayorinde@gmail.com>
2022-08-18 09:06:34 +0100

    added registerdrone endpoint implementation

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/controllers/DroneController.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/controllers/DroneController.java
@@ -1,6 +1,13 @@
 package com.ayorinde.dronetechapi.dronetechapi.controllers;
 
+import com.ayorinde.dronetechapi.dronetechapi.models.Drone;
+import com.ayorinde.dronetechapi.dronetechapi.requests.DroneRegistrationRequest;
+import com.ayorinde.dronetechapi.dronetechapi.responses.ApiResponse;
 import com.ayorinde.dronetechapi.dronetechapi.services.DroneServiceImpl;
+import com.ayorinde.dronetechapi.dronetechapi.util.Constants;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +18,14 @@ public class DroneController {
         this.droneService = droneService;
     }
 
+    @PostMapping("/api/droneTech/registerdrone")
+    public ResponseEntity Register(@RequestBody DroneRegistrationRequest droneRegistrationRequest) {
+        //Check drone weight
+        if (droneRegistrationRequest.getDroneWeight() > Constants.DRONE_WEIGHT) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Maximum Drone Weight is " + Constants.DRONE_WEIGHT, droneRegistrationRequest));
+        }
+        Drone drone = droneService.registerDrone(droneRegistrationRequest);
+        //return ResponseEntity.ok(d);
+        return ResponseEntity.ok(new ApiResponse(true, "Drone registered successfully!", drone));
+    }
 }
