69177d0e7500087c6312a9b81e3b44d73c59428d
adunseayorinde <adunseayorinde@gmail.com>
2022-08-18 13:16:48 +0100

    added dependencies for springfox and swaggerui

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/swagger/SpringFoxConfig.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/swagger/SpringFoxConfig.java
@@ -2,12 +2,15 @@ package com.ayorinde.dronetechapi.dronetechapi.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
+@EnableSwagger2WebMvc
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
@@ -16,5 +19,14 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+   // @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
