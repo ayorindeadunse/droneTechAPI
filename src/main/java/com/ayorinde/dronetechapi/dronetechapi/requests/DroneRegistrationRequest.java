3a4d1c1f392d9afc0b26b3d3d50cd0e12a60c229
adunseayorinde <adunseayorinde@gmail.com>
2022-08-23 09:06:20 +0100

    updated DroneRegistrationRequest DTO

--- src/main/java/com/ayorinde/dronetechapi/dronetechapi/requests/DroneRegistrationRequest.java
+++ src/main/java/com/ayorinde/dronetechapi/dronetechapi/requests/DroneRegistrationRequest.java
@@ -3,12 +3,15 @@ package com.ayorinde.dronetechapi.dronetechapi.requests;
 import com.ayorinde.dronetechapi.dronetechapi.models.DroneModel;
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public class DroneRegistrationRequest {
     @Digits(message="Number should contain 3 digits.", fraction = 0, integer = 10)
     private int droneWeight;
     private DroneModel droneModel;
+    @NotEmpty
     public String createSerialNumber()
     {
         String uniqueID = UUID.randomUUID().toString();
